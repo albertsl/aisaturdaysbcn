996b3d4b6f3872e8dfb3f2571bc655ae7d9b95ce
Albert <albertsanchezlafuente@gmail.com>
2018-03-24 14:04:47 +0100

    Added new python template and modified jupyter notebook template

---
+++ templates/python.py
@@ -0,0 +1,11 @@
+#Imports for data science
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def main():
+	pass
+
+if __name__ == "__main__":
+	main()
